f3fcaaf592ec788a17df6d61cb8fda729755ff81
nathanrowe4 <nathanrowe@bell.net>
2019-07-22 10:00:48 -0400

    Add function to block/unblock tasks

--- blocking_semaphore.h
+++ blocking_semaphore.h
@@ -10,6 +10,35 @@ void init(osBetaSemaphore_t *s, uint32_t count) {
     
 }
 
+//BLOCK
+void blockTask(osBetaSemaphore_t *s) {
+    
+    osBetaThread_t *cursor = s->waitList;
+    
+    if(cursor == NULL)
+        s->waitList = runningTask;
+    else {
+        while(cursor->next != NULL)
+            cursor = cursor->next;
+        
+        cursor->next = runningTask;
+    }
+    
+}
+
+//UNBLOCK
+void unblockTask(osBetaSemaphore_t *s) {
+    
+    if(s->waitList == NULL)
+        return;
+    else {
+        osBetaThread_t *thread = s->waitList;
+        s->waitList = thread->next;
+        addThreadToScheduler(thread);
+    }
+    
+}
+
 //WAIT
 void wait(osBetaSemaphore_t *s) {
     __disable_irq();
@@ -17,7 +46,7 @@ void wait(osBetaSemaphore_t *s) {
     (*s)--;
     
     if( *s >= 0) {
-        //block task
+        blockTask(s);
     }
     
     __enable_irq();
@@ -30,7 +59,7 @@ void signal(osBetaSemaphore_t *s) {
     (*s)++;
     
     if( *s <= 0 ) {
-        //unblock task
+        unblockTask(s);
     }
     
     __enable_irq();
